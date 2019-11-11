cfbbbe28fbdef2f022fc9e47f7f8d6f99f49d493
조규홍 <marioz9@naver.com>
2019-11-11 17:56:27 +0900

    191111 first

---
+++ 06_vue/04_vue_youtube_project/youtube-browser/src/components/VideoListItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <li @click="onVideoSelect" class="list-group-item">
+      <img :src="thumbnailUrl" alt="image" />
+      <div class="media-body" v-html="video.snippet.title"></div>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoListItem",
+  props: {
+    video: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    onVideoSelect() {
+      this.$emit("videoSelect", this.video); // $emit('videoSelect', ) videoSelect 올릴 이벤트 이름
+    }
+  },
+  computed: {
+    // 미리 캐싱
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    }
+  }
+};
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  cursor: pointer;
+}
+
+li:hover {
+  background-color: antiquewhite;
+}
+</style>
