ee4245cccca075cb46a216c7f78f47d83700d47c
조규홍 <marioz9@naver.com>
2019-11-20 13:44:43 +0900

    191120 first

--- 06_vue/05_vue_component_with_drf/todo-front/src/components/LoginForm.vue
+++ 06_vue/05_vue_component_with_drf/todo-front/src/components/LoginForm.vue
@@ -48,8 +48,13 @@
           username: '',
           password: '',
         },
-        loading: false,
+        // loading: false,
         errors: [],
+      }
+    },
+    computed: {
+      loading: function() {
+        return this.$store.state.loading
       }
     },
     methods: {
@@ -57,19 +62,24 @@
         if (this.checkForm()) {
           // 1. django jwt 를 생성하는 주소로 요청을 보냄
           // 이때 post 요청으로 보내야하며 사용자가 입력한 로그인 정보(credentials)를 같이 넘겨야 함
+          // this.loading = true
+          this.$store.dispatch('startLoading')
           axios.post('http://127.0.0.1:8000/api-token-auth/', this.credentials)
           .then(res => {
-            this.$session.start()
-            this.$session.set('jwt', res.data.token)
+            // this.$session.start()
+            // this.$session.set('jwt', res.data.token)
             // import router from '../router' 하고 사용한다. '/' 는 index.js 에서 home path
+            this.$store.dispatch('endLoading')
+            this.$store.dispatch('login', res.data.token)
             router.push('/')
             // 2. 로그인 이후에 loading 의 상태를 다시 false 로 변경
             // 그래야 spinner 가 계속 돌지 않고 로그인 form 을 받아 볼 수 있음
-            this.loading = false
+            // this.loading = false
           })
           .catch(err => {
             // 3. 로그인 실패 시 loading 의 상태를 다시 false 로 변경
-            this.loading = false
+            // this.loading = false
+            this.$store.dispatch('endLoading')
             console.log(err)
           })
         } else {
