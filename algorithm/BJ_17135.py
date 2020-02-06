cbd1a6f7fbb401b7d31170e19068bbc66be97cd8
91hongppie <91hongppie@gmail.com>
2020-02-06 19:58:44 +0900

    200206 algorithm

--- algorithm/BJ_17135.py
+++ algorithm/BJ_17135.py
@@ -1,7 +1,66 @@
 import sys
 sys.stdin = open('BJ_17135.txt', 'r')
+from pprint import pprint
+from collections import deque
+from copy import deepcopy
+
+def comb(num, nums):
+    if num > c:
+        return
+    if len(nums) == 3:
+        nums = nums[::]
+        comb_list.append(nums)
+        return nums
+    nums.append(num)
+    comb(num+1, nums)
+    nums.pop()
+    comb(num+1, nums)
+
+def war(archers, count, pan):
+    global result
+    for _ in range(r):
+        enemy_list = set()
+        for archer in archers:
+            find(archer, dis, enemy_list)
+        for k in enemy_list:
+            row, col = k
+            pan[row][col] = 0
+            count += 1
+        pan.pop()
+        pan.appendleft([0 for _ in range(c)])
+    result = max(result, count)
+
+def find(ar, dis, enemy):
+    Q.append([r, ar, dis])
+    while Q:
+        x, y, distance = Q.pop()
+        for u in range(3):
+            x1 = x + dx[u]
+            y1 = y + dy[u]
+            if distance > 0:
+                if 0 <= x1 < r and 0 <= y1 < c:
+                    if visit[x1][y1] == 0:
+                        Q.append([x1, y1, distance-1])
+                    else:
+                        enemy.add((x1, y1))
+                        return
+
+            
 
 r, c, dis = map(int, input().split())
-board = []
+dx = [0, -1, 0]
+dy = [-1, 0, 1]
+board = deque()
+Q = deque()
 for _ in range(r):
     board.append(list(map(int, input().split())))
+
+comb_list = []
+result = 0
+num_list = []
+comb(0, num_list)
+result = 0
+for u in comb_list:
+    visit = deepcopy(board)
+    war(u, 0, visit)
+print(result)
