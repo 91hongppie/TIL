8b5da428e042f39474c9c0a7a344513e0ab6f87f
91hongppie <91hongppie@gmail.com>
2020-02-25 17:39:16 +0900

    200225 home study

--- algorithm/BOJ_2573.py
+++ algorithm/BOJ_2573.py
@@ -2,40 +2,54 @@ import sys
 sys.stdin = open('BOJ_2573.txt', 'r')
 from collections import deque
 
-def BFS(r, c):
-    return 
+def DFS(r1, c1):
+    Q.append([r1, c1])
+    while Q:
+        x, y = Q.popleft()
+        for l in range(4):
+            x1 = x + dx[l]
+            y1 = y + dy[l]
+            if 0 <= x1 < r and 0 <= y1 < c:
+                if board[x1][y1] > 0:
+                    if island_list.index([x1, y1]):
+                        ind = island_list.index([x1, y1])
+                        if visit[ind] == False:
+                            visit[ind] = True
+                            Q.append([x1, y1])
+
 
 r, c = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(r)]
-visit = [[False] * c for _ in range(r)]
+Q = deque()
 island_list = []
-times = 0
+result = 0
+result_num = 0
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
-for i in range(1, r-1):
-    times = max(times, max(board[i]))
-    for j in range(1, c-1):
-        if board[i][j] != 0:
+for i in range(1, r):
+    for j in range(1, c):
+        if board[i][j] > 0:
             island_list.append([i, j])
-        else:
-            visit[i][j] = True
-for k in range(times):
-    n = 0
+            result = max(result, board[i][j])
+for ku in range(1, result+1):
+    poplist = []
     for u in range(len(island_list)):
-        for k in range(4):
-            if board[island_list[u][0]][island_list[u][1]] == 0:
-                visit[island_list[u][0]][island_list[u][1]] = True
-                island_visit = [False] * len(island_list)
-                for b in range(len(island_list)):
-                    if board[island_list[b][0]][island_list[b][1]] > 0:
-                        BFS(island_list[b][0], island_list[b][1])
+        board[island_list[u][0]][island_list[u][1]] -= 1
+        if board[island_list[u][0]][island_list[u][1]] == 0:
+            poplist.append(u)
+    if len(poplist) > 0:
+        for e in range(len(poplist)-1, -1, -1):
+            island_list.pop(poplist[e])
+        visit = [False] * len(island_list)
+        visit[0] = True
+        DFS(island_list[0][0], island_list[0][1])
+        nums = visit.count(False)
+        if nums != 0:
+            print(ku)
+            result_num = 1
+            break
+if result_num == 0:
+    print(0)
 
-
-                        
-                break
-            x1 = island_list[u][0] + dx[k]
-            y1 = island_list[u][1] + dy[k]
-            if 0 <= x1 < r and 0 <= y1 < c:
-                if board[x1][y1] == 0:
-                    board[island_list[u][0]][island_list[u][1]] -= 1
+    
 
