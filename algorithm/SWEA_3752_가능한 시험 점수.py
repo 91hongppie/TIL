054d1ee8c7faf04c1d7b2d807bcf7aff405bd145
91hongppie <91hongppie@gmail.com>
2020-03-07 13:34:01 +0900

    200307 algorithm home

--- algorithm/SWEA_3752_가능한 시험 점수.py
+++ algorithm/SWEA_3752_가능한 시험 점수.py
@@ -1,14 +1,5 @@
 import sys
 sys.stdin = open('SWEA_3752_가능한 시험 점수.txt', 'r')
-from collections import deque
-
-def grade(num, score):
-    if num == problems:
-        score_board[score] = 1
-        return
-    grade(num+1, score+scores[num])
-    grade(num+1, score)
-
 
 N = int(input())
 
@@ -16,7 +7,12 @@ for tc in range(1, N+1):
     problems = int(input())
     scores = list(map(int, input().split()))
     score_board = [0] * (sum(scores) + 1)
-    print(score_board)
-    score_list = set()
-    grade(0, 0)
-    print(score_board)
+    score_board[0] = 1
+    score_list = [0]
+    for u in scores:
+        p = len(score_list)
+        for k in score_list[:p]:
+            if score_board[u+k] == 0:
+                score_board[u+k] = 1
+                score_list.append(u+k)
+    print('#{} {}'.format(tc, len(score_list)))
