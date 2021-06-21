714ae223b4499f820d0c475e7d46dc9bc39deddc
91hongppie <91hongppie@gmail.com>
2021-06-21 23:35:39 +0900

    TIL Algorithm 210621

--- algorithm/BOJ_1107_리모컨.py
+++ algorithm/BOJ_1107_리모컨.py
@@ -2,43 +2,47 @@ import sys
 sys.stdin = open('BOJ_1107_리모컨.txt', 'r')
 
 
+def findChannel(fromChannel, toChannel, num):
+    global result
+    if int(num) not in breakBtnList:
+        newFromChannel = fromChannel + num
+        calcResult(newFromChannel, toChannel)
+        if len(newFromChannel) < len(toChannel):
+            findChannel(newFromChannel, toChannel, toChannel[len(newFromChannel)])
+    else:
+        q = 0
+        c = 0
+        while True:
+            if 0 <= int(num)-q < 10:
+                if int(num)-q == 0:
+                    c += 1
+                if int(num) - q not in breakBtnList:
+                    findChannel(fromChannel, toChannel, str(int(num) - q))
+            if 0 <= int(num)+q < 10:
+                if int(num) + q == 9:
+                    c += 1
+                if int(num) + q not in breakBtnList:
+                    findChannel(fromChannel, toChannel, str(int(num) + q))
+            if c == 2:
+                break
+            q += 1
+def calcResult(fromChannel, toChannel):
+    global result
+    tempCount = len(fromChannel) + abs(int(fromChannel) - int(toChannel))
+    result = min(result, tempCount)
+    return
+
 channelNum = int(input())
 breakNum = int(input())
 breakBtnList = list(map(int, input().split()))
 if channelNum == 100:
     print(0)
 elif len(breakBtnList) == 10:
-    print(channelNum - 100)
-elif channelNum - 100 < len(str(channelNum)):
-    print(channelNum - 100)
+    print(abs(channelNum - 100))
+elif abs(channelNum - 100) < len(str(channelNum)):
+    print(abs(channelNum - 100))
 else:
-    nowChannel = ''
+    result = abs(channelNum - 100)
     channelStr = str(channelNum)
-    count = 0
-    for i in channelStr:
-        if int(i) not in breakBtnList:
-            nowChannel += i
-            count += 1
-        else:
-            j = 1
-            while True:
-                btnCount = 0
-                if 0 <= abs(int(i)-j) < 10 and abs(int(i) - j) not in breakBtnList:
-                    nowChannel += str(abs(int(i) - j))
-                    count += 1
-                    break
-                if 0 <= abs(int(i)+j) < 10 and abs(int(i) + j) not in breakBtnList:
-                    nowChannel += str(abs(int(i) + j))
-                    count += 1
-                    break
-                if abs(int(i) + j) == 9 or abs(int(i) - j) == 0:
-                    btnCount += 1
-                    if btnCount == 2:
-                        break
-                j += 1
-    if channelNum - 100 > count + abs(int(nowChannel) - int(channelStr)):
-        print(count + abs(int(nowChannel) - int(channelStr)))
-    else:
-        print(channelNum - 100)
-                    
-                    
+    findChannel('', channelStr, channelStr[0])
+    print(result)
