0faee7d1cc0b55d5e51e2d6a5d9d6dbb17bff6e4
91hongppie <91hongppie@gmail.com>
2021-07-23 17:57:29 +0900

    TIL CS TransportLayer 210723

--- algorithm/BOJ_1238_파티.py
+++ algorithm/BOJ_1238_파티.py
@@ -2,52 +2,52 @@ import sys
 sys.stdin = open('BOJ_1238_파티.txt', 'r')
 
 from collections import deque
+
+
 N, M, X = list(map(int, input().split()))
+
+FromX = [[] for _ in range(N+1)]
+ToX = [[] for _ in range(N+1)]
 board = [[0 for _ in range(N+1)] for _ in range(N+1)]
 stack = [[] for _ in range(N+1)]
 
-def BFS(start, end, board, stack):
-    Q = deque()
-    Q.append([start, 0])
-    while Q:
-        newStart, dis = Q.popleft()
-        if board[start][end] != 0 and dis >= board[start][end]:
-            continue
-        if board[newStart][end] != 0:
-            newStart = end
-            dis += board[newStart][end]
-        if newStart == end:
-            if board[start][end] == 0:
-                board[start][end] = dis
-            else:
-                board[start][end] = min(board[start][end], dis)
-        else:
-            for i in stack[newStart]:
-                Q.append([i[0], dis+i[1]])
-
-
-for i in range(M):
+for _ in range(M):
     start, end, dis = list(map(int, input().split()))
-    if start == X:
-        board[start][end] = dis
-    elif end == X:
-        board[start][end] = dis
-    stack[start].append([end, dis])
-# for i in board:
-#     print(i)
+    FromX[end].append([start, dis])
+    ToX[start].append([end, dis])
+def BFS(toNum, board, disArray, num):
+    Q = deque()
+    Q.append([toNum, 0])
+    if num == 1:
+        fromNum = toNum
+    while Q:
+        if num == 0:
+            fromNum, dis = Q.popleft()
+        else:
+            toNum, dis = Q.popleft()
+        
+        if board[fromNum][toNum] == 0:
+            board[fromNum][toNum] = dis
+        else:
+            if dis >= board[fromNum][toNum]:
+                continue
+            else:
+                board[fromNum][toNum] = dis
+        if num == 0:
+            for i in disArray[fromNum]:
+                newToNum, newDis = i
+                Q.append([newToNum, dis+newDis])
+        else:
+            for i in disArray[toNum]:
+                newToNum, newDis = i
+                Q.append([newToNum, dis+newDis])
 
+
+BFS(X, board, FromX, 0)
+BFS(X, board, ToX, 1)
+board[X][X] = 0
+result = []
 for i in range(1, N+1):
-    if i == X:
-        continue
-    if board[i][X] == 0:
-        BFS(i, X, board, stack)
-    if  board[X][i] == 0:
-        BFS(X, i, board, stack)
-
-# print('')
-totalDis = [0 for _ in range(N+1)]
-for i in range(1, N+1):
-    totalDis[i] = board[i][X] + board[X][i]
-    # print(board[i])
-print(max(totalDis))
-
+    totalDis = board[i][X] + board[X][i]
+    result.append(totalDis)
+print(max(result))
